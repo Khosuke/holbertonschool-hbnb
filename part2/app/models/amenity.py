5d7166478e7262daf218ac2d3f3f7f7cc978e0f5
SaLaam17 <slaamri@yahoo.fr>
2025-02-24 14:15:00 +0100

    Implement Amenity class and Relationships Between Amenity and Place classes

---
+++ part2/app/models/amenity.py
@@ -0,0 +1,25 @@
+"""Module that defines the Amenity class."""
+
+from .base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Amenity class that inherits from BaseModel.
+    Args:
+        -name (String): The name of the amenity (e.g., "Wi-Fi", "Parking").
+        Required, maximum length of 50 characters.
+        -place (list): List of places associated with the amenity.
+    """
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+        self.place = []
+
+    def add_place(self, place):
+        """
+        method that add a place to an amenity
+        Args:
+            -place (Place): The place to add.
+        """
+        self.place.append(place)
