c232541d5bb55c9e22bbc029d47c234f9549a97e
Vitushan <vitushan@outlook.fr>
2025-02-26 16:50:36 +0100

    wahou

--- part2/app/api/v1/__init__.py
+++ part2/app/api/v1/__init__.py
@@ -1,13 +1,1 @@
-#!/usr/bin/python3
-
-
-from flask_restx import Api
-from .users import api as users_ns
-
-api = Api(
-    title="My API",
-    version="1.0",
-    description="A simple API",
-)
-
-api.add_namespace(users_ns, path="/users")
+ 
--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,5 +1,6 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
+from app.models.place import Place
 import uuid
 from flask_restx import abort
 
@@ -14,24 +15,9 @@ class HBnBFacade:
         """
         create a new user and store in memory
         """
-        existing_user = self.get_user_by_email(user_data['email'])
-        if existing_user:
-            abort(400, message="Email already registered")
-
-        user_id = str(uuid.uuid4())
-        user = User(id=user_id, **user_data)
+        user = User(**user_data)
         self.user_repo.add(user)
         return user 
-
-    def get_user_by_email(self, email):
-        """
-        Fetch user by email 
-        """
-        return self.user_repo.get_by_attribute('email', email)
-    
-    def get_place(self, place_id):
-
-        pass
 
     def get_user(self, user_id):
         """
@@ -43,6 +29,13 @@ class HBnBFacade:
             abort(404, message="User not found")
         return user
 
+    def get_user_by_email(self, email):
+        """
+        Fetch user by email 
+        """
+        return self.user_repo.get_by_attribute('email', email)
+    
+
     def put_user(self, user_id, user):
         """
         update existing users and
@@ -50,7 +43,7 @@ class HBnBFacade:
         """
         existing_user = self.get_user(user_id)
         if not existing_user:
-            abort(404, message="User not found")
+           return {"Error": str(e)}, 404
         
         user_data = {
             'first_name': user.first_name,
@@ -61,9 +54,35 @@ class HBnBFacade:
         if 'email' in user_data:
             existing_user_by_email = self.get_user_by_email(user_data['email'])
             if existing_user_by_email and existing_user_by_email.id != user_id:
-                abort(400, message="Email is already registered with another user")
+                return {"Error": str(e)}, 400
         try:
             self.user_repo.update(existing_user.id, user_data)
         except Exception as e:
-            abort(500, message=f"Error updating user: {e}")
+            return {"Error": str(e)}, 400
 
+    def create_place(self, place_data):
+        """
+        create place
+        """
+        place = Place(**place_data)
+        self.place_repo.add(place)
+        return place
+
+    def get_place(self, place_id):
+        """
+        get place by ID
+        """
+        place = self.place_repo.get(place_id)
+        if not place:
+            abort(404, message="Place not found")
+        return place
+
+    def get_all_places(self):
+    # Placeholder for logic to retrieve all places
+        pass
+
+    def update_place(self, place_id, place_data):
+        """
+        updating places
+        """
+      
