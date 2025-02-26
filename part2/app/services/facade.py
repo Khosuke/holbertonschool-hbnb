da25b5184403cb17685882d16eda5ed8c762d476
SaLaam17 <slaamri@yahoo.fr>
2025-02-26 16:10:21 +0100

    implementation of app/__init__.py, users.py and facade.py

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -9,21 +9,17 @@ class HBnBFacade:
         self.amenity_repo = InMemoryRepository()
 
     def create_amenity(self, amenity_data):
-        # Placeholder for logic to create an amenity
         amenity = Amenity(**amenity_data)
         self.amenity_repo.add(amenity)
         return amenity
 
     def get_amenity(self, amenity_id):
-        # Placeholder for logic to retrieve an amenity by ID
         return self.amenity_repo.get(amenity_id)
 
     def get_all_amenities(self):
-        # Placeholder for logic to retrieve all amenities
         return self.amenity_repo.get_all()
 
     def update_amenity(self, amenity_id, amenity_data):
-        # Placeholder for logic to update an amenity
             self.amenity_repo.update(amenity_id, amenity_data)
             return self.amenity_repo.get(amenity_id)
 
