2ce511a28c93b1c1e9e865d4de3a2124d8207895
Vitushan <vitushan@outlook.fr>
2025-03-10 14:01:00 +0100

    task2

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -4,6 +4,10 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from flask_jwt_extended import JWTManager
+
+jwt = JWTManager()
+
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
