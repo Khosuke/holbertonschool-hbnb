9a3ac4842e5ed59e4a25a7b420010e9dfbba4cbd
Vitushan <vitushan@outlook.fr>
2025-03-12 14:34:45 +0100

    replaces databases

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -7,10 +7,12 @@ from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.auth import api as auth_ns
+from flask_sqlalchemy import SQLAlchemy
+
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
-
+db = SQLAlchemy()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
@@ -28,9 +30,20 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(auth_ns, path='/api/v1/auth')
-
+    db.init_app(app)
     return app
 
 if __name__ == "__main__":
     app = create_app()
     app.run(debug=True)
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app(config_class=config.DevelopmentConfig):
+    #
+    # Existent code with app Flask instance
+    #
+    db.init_app(app)
