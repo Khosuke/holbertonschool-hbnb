138d7ff634dc45e68fb3cdc3625de1b828c9ee3a
Khosuke <9814@holbertonstudents.com>
2025-03-13 12:03:00 +0100

    updated user and place to implement a database with table and columns, with a new baseclass

--- part3/app/models/place.py
+++ part3/app/models/place.py
@@ -1,7 +1,22 @@
-from .basemodel import BaseModel
-from .user import User
+from app import db
+import uuid
+from .baseclass import BaseModel
+from app.models.user import User
 
 class Place(BaseModel):
+    """
+    __tablename__ = "Places"
+    
+    _title = db.Column(db.String(100), nullable=False)
+    _description = db.Column(db.String(300), nullable=False)
+    _price = db.Column(db.Float, nullable=False)
+    _latitude = db.Column(db.Float, nullable=False)
+    _longitude = db.Column(db.Float, nullable=False)
+    _owner = db.Column()
+    _reviews = db.Column(db.String(100), nullable=False)
+    _amenities = db.Column()
+    """
+
     def __init__(self, title, price, latitude, longitude, owner, description=None):
         super().__init__()
         self.title = title
@@ -15,7 +30,7 @@ class Place(BaseModel):
 
     @property
     def title(self):
-        return self.__title
+        return self._title
     
     @title.setter
     def title(self, value):
@@ -24,11 +39,11 @@ class Place(BaseModel):
         if not isinstance(value, str):
             raise TypeError("Title must be a string")
         super().is_max_length('title', value, 100)
-        self.__title = value
+        self._title = value
 
     @property
     def price(self):
-        return self.__price
+        return self._price
     
     @price.setter
     def price(self, value):
@@ -36,39 +51,39 @@ class Place(BaseModel):
             raise TypeError("Price must be a float")
         if value < 0:
             raise ValueError("Price must be positive.")
-        self.__price = value
+        self._price = value
 
     @property
     def latitude(self):
-        return self.__latitude
+        return self._latitude
     
     @latitude.setter
     def latitude(self, value):
         if not isinstance(value, float):
             raise TypeError("Latitude must be a float")
         super().is_between("latitude", value, -90, 90)
-        self.__latitude = value
+        self._latitude = value
     
     @property
     def longitude(self):
-        return self.__longitude
+        return self._longitude
     
     @longitude.setter
     def longitude(self, value):
         if not isinstance(value, float):
             raise TypeError("Longitude must be a float")
         super().is_between("longitude", value, -180, 180)
-        self.__longitude = value
+        self._longitude = value
 
     @property
     def owner(self):
-        return self.__owner
+        return self._owner
     
     @owner.setter
     def owner(self, value):
         if not isinstance(value, User):
             raise TypeError("Owner must be a user instance")
-        self.__owner = value
+        self._owner = value
 
     def add_review(self, review):
         """Add a review to the place."""
--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,44 +1,43 @@
-from .basemodel import BaseModel
+from .baseclass import BaseModel
 import re
 
-class User(BaseModel):
-    emails = set()
 
-    def __init__(self, first_name, last_name, email, password, is_admin=False):
-        super().__init__()
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.is_admin = is_admin
-        self.password = password
-        self.places = []
-        self.reviews = []
-    
+class User(BaseModel):
+    from app import db
+    emails = set()
+    __tablename__ = 'users'
+
+    _first_name = db.Column(db.String(50), nullable=False)
+    _last_name = db.Column(db.String(50), nullable=False)
+    _email = db.Column(db.String(120), nullable=False, unique=True)
+    _password = db.Column(db.String(128), nullable=False)
+    _is_admin = db.Column(db.Boolean, default=False)
+
     @property
     def first_name(self):
-        return self.__first_name
+        return self._first_name
     
     @first_name.setter
     def first_name(self, value):
         if not isinstance(value, str):
             raise TypeError("First name must be a string")
         super().is_max_length('First name', value, 50)
-        self.__first_name = value
+        self._first_name = value
 
     @property
     def last_name(self):
-        return self.__last_name
+        return self._last_name
 
     @last_name.setter
     def last_name(self, value):
         if not isinstance(value, str):
             raise TypeError("Last name must be a string")
         super().is_max_length('Last name', value, 50)
-        self.__last_name = value
+        self._last_name = value
 
     @property
     def email(self):
-        return self.__email
+        return self._email
 
     @email.setter
     def email(self, value):
@@ -48,30 +47,30 @@ class User(BaseModel):
             raise ValueError("Invalid email format")
         if value in User.emails:
             raise ValueError("Email already exists")
-        if hasattr(self, "_User__email"):
-            User.emails.discard(self.__email)
-        self.__email = value
+        if hasattr(self, "_User_email"):
+            User.emails.discard(self._email)
+        self._email = value
         User.emails.add(value)
 
     @property
     def password(self):
-        return self.__password
+        return self._password
     
     @password.setter
     def password(self, value):
         if not isinstance(value, str):
             raise TypeError("Password must be a string")
-        self.__password = self.hash_password(value)
+        self._password = self.hash_password(value)
 
     @property
     def is_admin(self):
-        return self.__is_admin
+        return self._is_admin
     
     @is_admin.setter
     def is_admin(self, value):
         if not isinstance(value, bool):
             raise TypeError("Is Admin must be a boolean")
-        self.__is_admin = value
+        self._is_admin = value
 
     def add_place(self, place):
         """Add an amenity to the place."""
@@ -93,7 +92,7 @@ class User(BaseModel):
     def verify_password(self, password):
         """Verifies if the provided password matches the hashed password."""
         from app import bcrypt
-        return bcrypt.check_password_hash(self.__password, password)
+        return bcrypt.check_password_hash(self._password, password)
     
     def to_dict(self):
         return {
