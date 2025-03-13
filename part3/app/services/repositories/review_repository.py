ac4e04b1df0c389002d3002256d4390dbfc99701
Khosuke <9814@holbertonstudents.com>
2025-03-13 12:06:39 +0100

    removed comment line

--- part3/app/services/repositories/review_repository.py
+++ part3/app/services/repositories/review_repository.py
@@ -1,6 +1,5 @@
 from app.models.review import Review
 from app.persistence.repository import SQLAlchemyRepository
-# from app import db
 
 class ReviewRepository(SQLAlchemyRepository):
     def __init__(self, model):
